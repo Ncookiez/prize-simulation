363e22c9870595c9cc00c9b7be62e3e09bfa74ba
Ncookiez <ncukiert@gmail.com>
2022-05-17 18:26:13 -0300

    Multiple deposit value simulations

--- prizeSimulation.js
+++ prizeSimulation.js
@@ -1,17 +1,10 @@
 
-// Initializations:
-const maxPrizes = 2;
-let scaling = 1;
-let totalDailyPrizeAmount = 0;
-let totalDailyPrizeCount = 0;
-let dailyWinProbability = 0;
-let totalWinnings = 0;
-
 // Settings:
-const depositAmount = 25000;
-const tvl = 29000000;
+const depositAmounts = [10, 50, 100, 500, 750, 1000, 2000, 3000, 4000, 5000, 7500, 10000, 25000, 50000, 75000, 100000, 300000, 500000, 750000, 1000000];
+const tvl = 30000000;
+const maxPrizes = 2;
 const simulationDays = 365;
-const simulationRuns = 300;
+const simulationRuns = 1000;
 const prizeTiers = [
   { prize: 1000, num: 1 },
   { prize: 100, num: 3 },
@@ -25,43 +18,50 @@ const prizeTiers = [
 
 // Function to simulate prize APR:
 const simulatePrizeAPR = () => {
+  depositAmounts.forEach(depositAmount => {
+
+    // Initializations:
+    let scaling = 1;
+    let totalDailyPrizeCount = 0;
+    let dailyWinProbability = 0;
+    let totalWinnings = 0;
   
-  // Finding Appropriate Scaling For Efficiency:
-  if(depositAmount >= 50000) {
-    scaling = depositAmount / 500;
-  } else if(depositAmount >= 5000) {
-    scaling = depositAmount / 100;
-  } else if(depositAmount >= 50) {
-    scaling = depositAmount / 50;
-  }
+    // Finding Appropriate Scaling For Efficiency:
+    if(depositAmount >= 50000) {
+      scaling = depositAmount / 500;
+    } else if(depositAmount >= 5000) {
+      scaling = depositAmount / 100;
+    } else if(depositAmount >= 50) {
+      scaling = depositAmount / 50;
+    }
 
-  // Finding Total Prize Data:
-  prizeTiers.forEach(tier => {
-    totalDailyPrizeAmount += tier.prize * tier.num;
-    totalDailyPrizeCount += tier.num;
+    // Finding Total Prize Data:
+    prizeTiers.forEach(tier => {
+      totalDailyPrizeCount += tier.num;
+    });
+
+    // Finding Daily Winning Probability:
+    dailyWinProbability = 1 / (tvl / totalDailyPrizeCount / scaling);
+
+    // Running Simulations:
+    for(let i = 0; i < simulationRuns; i++) {
+      let winnings = simulate(depositAmount, scaling, totalDailyPrizeCount, dailyWinProbability);
+      totalWinnings += winnings;
+    }
+
+    // Calculating APR:
+    let avgWinnings = totalWinnings / simulationRuns;
+    let apr = (avgWinnings / depositAmount) * ((365 / simulationDays) * 100);
+    console.log(`Average APR for $${depositAmount}: ${apr.toFixed(2)}%`);
+
   });
-
-  // Finding Daily Winning Probability:
-  dailyWinProbability = 1 / (tvl / totalDailyPrizeCount / scaling);
-
-  // Running Simulations:
-  for(let i = 0; i < simulationRuns; i++) {
-    let winnings = simulate();
-    totalWinnings += winnings;
-  }
-
-  // Calculating APR:
-  let avgWinnings = totalWinnings / simulationRuns;
-  let apr = (avgWinnings / depositAmount) * ((365 / simulationDays) * 100);
-  console.log(`Average APR: ${apr.toFixed(2)}%`);
-
 }
 
 // Function to run a simulation:
-const simulate = () => {
+const simulate = (depositAmount, scaling, totalDailyPrizeCount, dailyWinProbability) => {
   let winnings = 0;
   for(let day = 0; day < simulationDays; day++) {
-    let dayResult = simulateDay();
+    let dayResult = simulateDay(depositAmount, scaling, totalDailyPrizeCount, dailyWinProbability);
     dayResult.forEach(prize => {
       winnings += prize;
     });
@@ -70,7 +70,7 @@ const simulate = () => {
 }
 
 // Function to simulate a day's results:
-const simulateDay = () => {
+const simulateDay = (depositAmount, scaling, totalDailyPrizeCount, dailyWinProbability) => {
   let prizes = [];
   for(let draw = 0; draw < Math.trunc(depositAmount / scaling); draw++) {
     prizeTiers.forEach(tier => {
